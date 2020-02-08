01441c2309bbb1369503493726349fa5c193abbb
DarrenRu <rocker13211@gmail.com>
2020-02-08 18:12:38 +0800

    1st

---
+++ res/data/menuData.js
@@ -0,0 +1,83 @@
+var menuData = {
+    "T": [
+        {
+            title: '學生資料管理系統' ,
+            link: 'javascript:void(0);',
+            command: 'TeacherStudentManager',
+            views: 'Student',
+            sub: null,
+            icon: null
+        },
+    
+    ],
+    //     {
+    //         title: '編輯考題 ',
+    //         link: 'javascript:void(0);',
+    //         command: 'EditExam',
+    //         views: 'UpLoadFile',
+    //         sub: null,
+    //         icon: null
+    //     },
+    //    {
+    //         title: '審查考題' ,
+    //        link: 'javascript:void(0);',
+    //        command: 'MonitorExam',
+    //        views: 'UpLoadFile',
+    //        sub: null,
+    //        icon: null
+    //    },
+    // ],
+    "P":[
+        {
+            title: '首頁' ,
+            link: 'javascript:void(0);',
+            command: 'userpage',
+            views: 'gondan',
+            sub: null,
+            icon: null
+        },
+       
+        // {
+        //     title: '進度' ,
+        //     link: 'javascript:void(0);',
+        //     command: 'userpage',
+        //     views: 'gondan',
+        //     sub: null,
+        //     icon: null
+        // },
+        {
+            title: '資源管理' ,
+            link: 'javascript:void(0);',
+            command: 'userpage2',
+            views: 'gondan',
+            sub: null,
+            icon: null
+        },
+        {
+            title: '訂單' ,
+            link: 'javascript:void(0);',
+            command: 'APS',
+            views: 'gondan',
+            sub: null,
+            icon: null
+        },
+        {
+            title: '生產管理' ,
+            link: 'javascript:void(0);',
+            command: 'Production',
+            views: 'ProductionManage',
+            sub: null,
+            icon: null
+        },
+        // {
+        //     title: '日程' ,
+        //     link: 'javascript:void(0);',
+        //     command: 'personal',
+        //     views: 'gondan',
+        //     sub: null,
+        //     icon: null
+        // }
+    ],
+    
+    
+};
---
+++ res/js/userpage2.js
@@ -0,0 +1,173 @@
+
+
+
+
+
+function init() {
+    // Create
+    $("#peopleCreate_Content").load("/res/modalCreate/peopleCreate.html");
+    $("#cutterCreate_Content").load("/res/modalCreate/cutterCreate.html");
+    $("#machineCreate_Content").load("/res/modalCreate/machineCreate.html");
+    $("#jigCreate_Content").load("/res/modalCreate/jigCreate.html");
+    $("#materialCreate_Content").load("/res/modalCreate/materialCreate.html");
+
+    // Edit
+    $("#peopleEdit_Content").load("/res/modalEdit/peopleEdit.html");
+    $("#cutterEdit_Content").load("/res/modalEdit/cutterEdit.html");
+    $("#machineEdit_Content").load("/res/modalEdit/machineEdit.html");
+    $("#jigEdit_Content").load("/res/modalEdit/jigEdit.html");
+    $("#materialEdit_Content").load("/res/modalEdit/materialEdit.html");
+
+}
+function peopleCreate() {
+    $("#peopleCreate").modal("show");
+}
+
+function cutterCreate() {
+    $("#cutterCreate").modal("show");
+}
+
+function machineCreate() {
+    $("#machineCreate").modal("show");
+}
+
+function materialCreate() {
+    $("#materialCreate").modal("show");
+}
+
+function jigCreate() {
+    $("#jigCreate").modal("show");
+}
+
+
+
+function peopleEdit(id) {
+    $("#peopleEdit_title").text(id + " 編輯頁面")
+    $("#peopleEdit").modal("show");
+}
+
+function machineEdit(id) {
+    $("#machineEdit_title").text(id + " 編輯頁面")
+    $("#machineEdit").modal("show");
+    $.ajax({
+        type: "POST",
+        headers: { 'Authorization': getCookie("token") },
+        url: webApiUrl + "/machine/getById",
+        contentType: "application/json;charset=utf-8",
+        async: false,
+        data: JSON.stringify({
+            'id': id,
+        }),
+        success: function (e) {   
+            $("#machineIdEdit").val(e.id);
+
+            $("#machineClassEdit").val(e.machineClass);
+            $("#machineNameEdit").val(e.machineName);
+            $("#machineNoEdit").val(e.machineNo);
+            $("#machineBrandEdit").val(e.machineBrand);
+            $("#machineSizeEdit").val(e.machineSize);
+
+        }
+
+    })
+   
+}
+
+function cutterEdit(id) {
+    $("#cutterEdit_title").text(id + " 編輯頁面")
+
+    $("#cutterEdit").modal("show");
+}
+
+
+
+function materialEdit(id) {
+    $("#materialEdit_title").text(id + " 編輯頁面")
+
+    $("#materialEdit").modal("show");
+}
+
+function jigEdit(id) {
+    $("#jigEdit_title").text(id + " 編輯頁面")
+
+    $("#jigEdit").modal("show");
+}
+
+function machineCreateContent() {
+    alert("ddd");
+    // <!-- public string machineClass { get; set; }
+    // public string machineName { get; set; }
+    // public int? machineNo { get; set; }
+    // public string machineBrand { get; set; }
+    // public string machineSize { get; set; }
+    // public byte? machineStatus { get; set; } -->
+    $.ajax({
+        type: "POST",
+        headers: { 'Authorization': getCookie("token") },
+        url: webApiUrl + "/machine/Create",
+        contentType: "application/json;charset=utf-8",
+        async: false,
+        data: JSON.stringify({
+            'machineClass': $('#machineClassCreate').val(),
+            'machineName': $('#machineNameCreate').val(),
+            'machineNo': $('#machineNoCreate').val(),
+            'machineBrand': $('#machineBrandCreate').val(),
+            'machineSize': $('#machineSizeCreate').val(),
+        }),
+        success: function () {
+            machineTable.draw();
+            $("#machineCreate").modal("hide");
+        }
+
+    })
+
+}
+
+function machineEditContent() {
+
+    $.ajax({
+        type: "POST",
+        headers: { 'Authorization': getCookie("token") },
+        url: webApiUrl + "/machine/Update",
+        contentType: "application/json;charset=utf-8",
+        async: false,
+        data: JSON.stringify({
+            'id': $('#machineIdEdit').val(),
+            'machineClass': $('#machineClassEdit').val(),
+            'machineName': $('#machineNameEdit').val(),
+            'machineNo': $('#machineNoEdit').val(),
+            'machineBrand': $('#machineBrandEdit').val(),
+            'machineSize': $('#machineSizeEdit').val(),
+        }),
+        success: function () {
+            machineTable.draw();
+            $("#machineEdit").modal("hide");
+        }
+    })
+}
+
+function machineDelete (id) {
+    var confirmStatus = confirm("是否刪除?");
+
+    if (confirmStatus)
+        $.ajax({
+            type: "POST",
+            headers: { 'Authorization': getCookie("token") },
+            url: webApiUrl + "/machine/delete",
+            contentType: "application/json;charset=utf-8",
+            async: false,
+            data: JSON.stringify({
+                'id': id,
+            }),
+            success: function () {
+                machineTable.draw();
+                alert("刪除成功")
+            },
+            error:function(){
+                alert("刪除失敗")
+            }
+
+        })
+
+}
+
